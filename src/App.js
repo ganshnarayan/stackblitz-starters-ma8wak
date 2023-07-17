3000b6bc3fdd2aedc10877186976701019daa751
ganshnarayan <127541155+ganshnarayan@users.noreply.github.com>
2023-07-17 20:31:39 +0530

    added name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello Stack</h1>
+      <h1>Hello Ganesh</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
